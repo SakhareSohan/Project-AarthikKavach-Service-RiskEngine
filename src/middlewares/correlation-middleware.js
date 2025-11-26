c8019e3cd12e3cd4151603e02663c9230c5f3c7b
sohan sakhare <sohan.sakhare@unicoconnect.com>
2025-11-26 10:00:42 +0530

    [Sohan] Modified: Correlation Middleware

--- src/middlewares/correlation-middleware.js
+++ src/middlewares/correlation-middleware.js
@@ -1,9 +1,11 @@
 const { randomUUID } = require('crypto');
 const { asyncLocalStorage } = require("../utils/helpers/request.helpers");
 
-function attachCorrelationIdMiddleware(req,res,next) {
-  const correlationId =randomUUID() ;
+function attachCorrelationIdMiddleware(req, res, next) {
+  const correlationId = req.headers['x-correlation-id'] || randomUUID();
   req.headers["x-correlation-id"] = correlationId;
+  req.correlationId = correlationId; // Attach to req object for easy access
+
   // Create an async stor that carries the correlationid for this requst(temp storage for request)
   asyncLocalStorage.run({ correlationId: correlationId }, () => {
     next();
